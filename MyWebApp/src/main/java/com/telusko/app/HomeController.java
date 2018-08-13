b519a35d4a215a36af933adf458e60a4010d74a7
Amar Singh <Amar Singh@Amarsingh>
2018-08-13 19:12:48 +0530

    First Commit
    Uploading All Files

---
+++ MyWebApp/src/main/java/com/telusko/app/HomeController.java
@@ -0,0 +1,20 @@
+package com.telusko.app;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+@RequestMapping("Home")
+public String home(HttpServletRequest req)
+{
+	HttpSession session = req.getSession();
+	String name= req.getParameter("name");
+	System.out.println("Hiiiiii....." + name);
+	session.setAttribute("name",name);
+	return "Home";
+}
+}
